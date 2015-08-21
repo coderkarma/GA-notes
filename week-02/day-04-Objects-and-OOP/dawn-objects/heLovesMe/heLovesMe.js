602202b6f44fdb98dd356f8662c39297ba7c96c4
Melissa M. Dailey <Mel@Ms-MacBook-Pro.local>
2015-08-21 09:51:20 -0700

    complete update of notes repository

---
+++ week-02/day-04-Objects-and-OOP/dawn-objects/heLovesMe/heLovesMe.js
@@ -0,0 +1,41 @@
+/*
+
+Create an object method for flower that will play the age old game 'He loves me, he loves me not...'
+
+Count down from the petal number down to 1
+Alternately display 'He loves me' or 'He loves me not' to the console for each petal count decrement.
+Display the final phrase with an exclamation; that's the end of the game!
+*/
+
+
+
+
+
+
+function Flower() {
+	this.color= "orange";
+	this.smellsPretty= "true";
+	this.petalCount= 32;
+	this.loveGame = function (this.petalCount) {
+		
+
+		})
+
+	}
+}
+
+
+function (this.petalCount) {
+	this.petalCount.forEach(function (petal) {
+			this.petalCount - 1;
+			console.log(this.petalCount);
+
+
+
+//this works
+function Flower() {
+	this.color= "orange";
+	this.smellsPretty= "true";
+	this.petalCount= 32;
+	console.log(this.petalCount);
+}
